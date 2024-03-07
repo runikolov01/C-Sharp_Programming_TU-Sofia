d93c84120fd4f259ff148bbf5acb9ecc850b08dc
Rumen Nikolov <rumenhco2001@abv.bg>
2024-03-07 09:46:28 +0200

    Create Task03.cs

---
+++ E02/Task03.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E02
+{
+    public class Task03
+    {
+        public static void Execute()
+        {
+        }
+    }
+}
