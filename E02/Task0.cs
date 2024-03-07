112f70a5ab46638ef5104abe7bdffbcbcef6af32
Rumen Nikolov <rumenhco2001@abv.bg>
2024-03-07 09:46:17 +0200

    Create Task0.cs

---
+++ E02/Task0.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace E02
+{
+    class Task0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Изберете задача номер:");
+            Console.WriteLine("1. Обработка на географски координати");
+            Console.WriteLine("2. Телефонни контакти");
+            Console.WriteLine("3. Чете се .xml файл (input3.dae), описващ 3D сцена");
+            Console.Write("Въведете номер на задачата: ");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Task01.Execute();
+                        break;
+                    case 2:
+                        Task02.Execute();
+                        break;
+                    case 3:
+                        Task03.Execute();
+                        break;
+                    default:
+                        Console.WriteLine("Невалиден избор.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Невалиден избор.");
+            }
+        }
+    }
+}
