654c92eaaca2fbed63a3e6e9b9d1d1a8cfe7b06e
Rumen Nikolov <rumenhco2001@abv.bg>
2024-03-07 11:18:58 +0200

    Exercise 2 is ready

--- E02/Task03.cs
+++ E02/Task03.cs
@@ -1,15 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
 
 namespace E02
 {
     public class Task03
     {
+        public class Tag
+        {
+            public string Name { get; set; }
+            public Dictionary<string, string> Attributes { get; set; }
+            public Tag Connection { get; set; } // Връзка към друг таг
+
+            public Tag()
+            {
+                Attributes = new Dictionary<string, string>();
+            }
+        }
+
         public static void Execute()
         {
+            string filePath = "input-03.dae"; // Пътят към XML файла
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+
+            XmlNode root = xmlDoc.DocumentElement;
+            Tag rootTag = ParseXmlNode(root);
+
+            string json = JsonConvert.SerializeObject(rootTag, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json);
+        }
+
+        // Рекурсивен метод за парсване на XML таговете и тяхната връзка
+        private static Tag ParseXmlNode(XmlNode node)
+        {
+            Tag tag = new Tag();
+            tag.Name = node.Name;
+
+            if (node.Attributes != null)
+            {
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    if (attribute.Value.StartsWith("#"))
+                    {
+                        // Ако атрибута започва с '#', търсим таг с такъв идентификатор
+                        string id = attribute.Value.Substring(1);
+                        XmlNode referencedNode = node.SelectSingleNode($"//*[@id='{id}']");
+                        if (referencedNode != null)
+                        {
+                            tag.Connection = ParseXmlNode(referencedNode);
+                        }
+                    }
+                    else
+                    {
+                        // В противен случай добавяме атрибута към текущия таг
+                        tag.Attributes.Add(attribute.Name, attribute.Value);
+                    }
+                }
+            }
+
+            // Рекурсивно парсваме всички деца на текущия таг
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                Tag childTag = ParseXmlNode(childNode);
+                // Добавяме детето като част от текущия таг
+                tag.Connection = childTag;
+            }
+
+            return tag;
         }
     }
 }
--- E02/obj/Debug/net8.0/E02.AssemblyInfo.cs
+++ E02/obj/Debug/net8.0/E02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f058212a05a9a8009c9eeecc720c6fb3723070d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cef2d615ed8639ec227469a496950ece6f0c301")]
 [assembly: System.Reflection.AssemblyProductAttribute("E02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
